9755d654c6dfa6becf0023d57e426f8c5e88f5cc
JinfeiChen <jinfei_chen@icloud.com>
2020-12-30 18:35:59 +0800

    update

---
+++ IMSForm/Classes/Libraries/IMSFormDataManager.h
@@ -0,0 +1,16 @@
+//
+//  IMSFormDataManager.h
+//  Pods
+//
+//  Created by cjf on 30/12/2020.
+//
+
+#import <IMSForm/IMSForm.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface IMSFormDataManager : IMSFormObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ IMSForm/Classes/Libraries/IMSFormDataManager.m
@@ -0,0 +1,12 @@
+//
+//  IMSFormDataManager.m
+//  Pods
+//
+//  Created by cjf on 30/12/2020.
+//
+
+#import "IMSFormDataManager.h"
+
+@implementation IMSFormDataManager
+
+@end
