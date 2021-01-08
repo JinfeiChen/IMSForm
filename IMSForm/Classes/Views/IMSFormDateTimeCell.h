f328d92b7beaf7df0d2f49c81bb0b0b9455a36a0
Clemmie <clemmie@imanagesystems.com>
2021-01-08 17:15:12 +0800

    dateTime file create

---
+++ IMSForm/Classes/Views/IMSFormDateTimeCell.h
@@ -0,0 +1,16 @@
+//
+//  IMSFormDateTimeCell.h
+//  IMSForm
+//
+//  Created by IMS_Mac on 2021/1/8.
+//
+
+#import <IMSForm/IMSForm.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface IMSFormDateTimeCell : IMSFormTableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
