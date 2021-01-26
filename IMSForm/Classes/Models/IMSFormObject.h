bd97b8d6f9fa2dbd86a4a07bd6a92ab49f11bfe2
JinfeiChen <jinfei_chen@icloud.com>
2021-01-26 15:41:02 +0800

    update basic property;

--- IMSForm/Classes/Models/IMSFormObject.h
+++ IMSForm/Classes/Models/IMSFormObject.h
@@ -14,7 +14,12 @@ NS_ASSUME_NONNULL_BEGIN
 
 @property (copy, nonatomic) NSString *identifier; /**< 唯一标识 */
 
-@property (assign, nonatomic, getter=isEnabled) BOOL enabled; /**< 可用性 */
+@property (assign, nonatomic, getter=isEnable) BOOL enable; /**< 可用性 */
+@property (assign, nonatomic, getter=isClearable) BOOL clearable; /**< 是否可清除 */
+@property (assign, nonatomic, getter=isEditable) BOOL editable; /**< 是否可编辑 */
+@property (assign, nonatomic, getter=isVisible) BOOL visible; /**< 是否可见 */
+@property (assign, nonatomic, getter=isRequired) BOOL required; /**< 是否必需 */
+@property (assign, nonatomic, getter=isSelected) BOOL selected; /**< 是否为选中状态 */
 
 @property (copy, nonatomic) NSString *label; /**< 用于数据显示 */
 
