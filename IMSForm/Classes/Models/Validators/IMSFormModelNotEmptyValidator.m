bfece1185fecdb462a06d349ba827c3c92a30dfe
JinfeiChen <jinfei_chen@icloud.com>
2021-02-07 22:30:34 +0800

    update empty validator;

--- IMSForm/Classes/Models/Validators/IMSFormModelNotEmptyValidator.m
+++ IMSForm/Classes/Models/Validators/IMSFormModelNotEmptyValidator.m
@@ -12,7 +12,34 @@
 
 - (BOOL)validateFormModel:(IMSFormModel *)formModel
 {
-    return formModel.value && (formModel.value.length != 0) && [formModel.value isKindOfClass:[NSString class]];
+    if (
+        [formModel.type isEqualToString:IMSFormComponentType_Select] ||
+        [formModel.type isEqualToString:IMSFormComponentType_MultiSelect] ||
+        [formModel.type isEqualToString:IMSFormComponentType_Cascader] ||
+        [formModel.type isEqualToString:IMSFormComponentType_Radio] ||
+        [formModel.type isEqualToString:IMSFormComponentType_FileUpload] ||
+        [formModel.type isEqualToString:IMSFormComponentType_ImageUpload] ||
+        [formModel.type isEqualToString:IMSFormComponentType_InputTag] ||
+        [formModel.type isEqualToString:IMSFormComponentType_InputSearch]
+        ) {
+        return formModel.valueList && (formModel.valueList.count > 0) && [formModel.valueList isKindOfClass:[NSArray class]];
+    }
+    else if (
+             [formModel.type isEqualToString:IMSFormComponentType_TextField] ||
+             [formModel.type isEqualToString:IMSFormComponentType_TextView] ||
+             [formModel.type isEqualToString:IMSFormComponentType_Number] ||
+             [formModel.type isEqualToString:IMSFormComponentType_Switch] ||
+             [formModel.type isEqualToString:IMSFormComponentType_Range] ||
+             [formModel.type isEqualToString:IMSFormComponentType_Currency] ||
+             [formModel.type isEqualToString:IMSFormComponentType_Phone] ||
+             [formModel.type isEqualToString:IMSFormComponentType_Slider] ||
+             [formModel.type isEqualToString:IMSFormComponentType_DateTimePicker]
+             ) {
+        return formModel.value && (formModel.value.length != 0) && [formModel.value isKindOfClass:[NSString class]];
+    }
+    else {
+        return formModel.value && (formModel.value.length != 0) && [formModel.value isKindOfClass:[NSString class]];
+    }
 }
 
 @end
