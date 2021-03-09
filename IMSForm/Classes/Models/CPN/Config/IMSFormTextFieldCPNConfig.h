0ce7e30a48ff1d289f108cd157af07cd3e2a2fe5
JinfeiChen <jinfei_chen@icloud.com>
2021-03-09 14:37:21 +0800

    1.fixed some bugs; 2.add property unit;

--- IMSForm/Classes/Models/CPN/Config/IMSFormTextFieldCPNConfig.h
+++ IMSForm/Classes/Models/CPN/Config/IMSFormTextFieldCPNConfig.h
@@ -14,8 +14,11 @@ NS_ASSUME_NONNULL_BEGIN
 @property (copy, nonatomic) IMSFormTextType textType; /**< 文本输入类型限制, e.g. phone/email/url... */
 @property (assign, nonatomic) NSInteger lengthLimit; /**< 文本最大输入长度限制, default 100 */
 @property (assign, nonatomic) NSInteger precision; /**< 小数点精确度(小数点位数), default 2 */
+
+@property (copy, nonatomic) NSString *unit; /**< 符号，默认为后置符号（等同于suffixUnit），设置后suffixUnit将失效 */
 @property (copy, nonatomic) NSString *prefixUnit; /**< 前置符号, e.g. $ */
 @property (copy, nonatomic) NSString *suffixUnit; /**< 后置符号, e.g. % */
+
 
 @end
 
