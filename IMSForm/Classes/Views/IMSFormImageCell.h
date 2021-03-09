c30389d5ac0b55a9779a2296c731d75605e0c669
JinfeiChen <jinfei_chen@icloud.com>
2021-03-09 16:29:13 +0800

    rename cell;

--- IMSForm/Classes/Views/IMSFormImageCell.h
+++ IMSForm/Classes/Views/IMSFormImageCell.h
@@ -11,7 +11,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface CJFFormTBImageUpload001CollectionViewCell : UICollectionViewCell
+@interface CJFFormImageItemCell : UICollectionViewCell
 
 @property (nonatomic, strong) UIImageView *imageView;
 @property (nonatomic, strong) UIImageView *videoImageView;
