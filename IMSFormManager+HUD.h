16c9820d8f8112751a14509775084a1fab1a1461
Jenkins <jenkins@imanagesystems.com>
2021-04-29 14:04:58 +0800

    update

--- IMSFormManager+HUD.h
+++ IMSFormManager+HUD.h
@@ -5,7 +5,7 @@
 //  Created by cjf on 29/4/2021.
 //
 
-#import <Foundation/Foundation.h>
+#import <IMSForm/IMSFormManager.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
--- IMSFormManager+HUD.m
+++ IMSFormManager+HUD.m
@@ -10,40 +10,40 @@
 
 @implementation IMSFormManager (HUD)
 
-- (void)showInfo:(NSString *)msg
-{
-    if (self.hudDelegate && [self.hudDelegate respondsToSelector:@selector(showInfo:)]) {
-        [self.hudDelegate showInfo:msg];
-        return;
-    }
-    [IMSDropHUD showAlertWithType:IMSFormMessageType_Info message:msg];
-}
-
-- (void)showSuccess:(NSString *)msg
-{
-    if (self.hudDelegate && [self.hudDelegate respondsToSelector:@selector(showSuccess:)]) {
-        [self.hudDelegate showSuccess:msg];
-        return;
-    }
-    [IMSDropHUD showAlertWithType:IMSFormMessageType_Success message:msg];
-}
-
-- (void)showError:(NSString *)msg
-{
-    if (self.hudDelegate && [self.hudDelegate respondsToSelector:@selector(showError:)]) {
-        [self.hudDelegate showError:msg];
-        return;
-    }
-    [IMSDropHUD showAlertWithType:IMSFormMessageType_Error message:msg];
-}
-
-- (void)showWarning:(NSString *)msg
-{
-    if (self.hudDelegate && [self.hudDelegate respondsToSelector:@selector(showWarning:)]) {
-        [self.hudDelegate showWarning:msg];
-        return;
-    }
-    [IMSDropHUD showAlertWithType:IMSFormMessageType_Warning message:msg];
-}
+//- (void)showInfo:(NSString *)msg
+//{
+//    if (self.hudDelegate && [self.hudDelegate respondsToSelector:@selector(showInfo:)]) {
+//        [self.hudDelegate showInfo:msg];
+//        return;
+//    }
+//    [IMSDropHUD showAlertWithType:IMSFormMessageType_Info message:msg];
+//}
+//
+//- (void)showSuccess:(NSString *)msg
+//{
+//    if (self.hudDelegate && [self.hudDelegate respondsToSelector:@selector(showSuccess:)]) {
+//        [self.hudDelegate showSuccess:msg];
+//        return;
+//    }
+//    [IMSDropHUD showAlertWithType:IMSFormMessageType_Success message:msg];
+//}
+//
+//- (void)showError:(NSString *)msg
+//{
+//    if (self.hudDelegate && [self.hudDelegate respondsToSelector:@selector(showError:)]) {
+//        [self.hudDelegate showError:msg];
+//        return;
+//    }
+//    [IMSDropHUD showAlertWithType:IMSFormMessageType_Error message:msg];
+//}
+//
+//- (void)showWarning:(NSString *)msg
+//{
+//    if (self.hudDelegate && [self.hudDelegate respondsToSelector:@selector(showWarning:)]) {
+//        [self.hudDelegate showWarning:msg];
+//        return;
+//    }
+//    [IMSDropHUD showAlertWithType:IMSFormMessageType_Warning message:msg];
+//}
 
 @end
